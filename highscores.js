d2dada4d7a199cb79cd9445c6645a8650e4590d8
Omkar <omkarhadge5@gmail.com>
2022-04-14 13:59:35 +0530

    initial

--- highscores.js
+++ highscores.js
@@ -1,8 +1,14 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+var index=0;
 
 highScoresList.innerHTML = highScores
   .map(score => {
-    return `<li class="high-score">${score.score} - ${score.name}</li>`;
+    console.log(score);
+    return `<div class="main-box">
+    <div class="high-score">${++index} </div>
+    <div class="high-score">${score.score}</div>
+    <div class="high-score name">${score.name}</div>
+    </div>`;
   })
   .join("");
